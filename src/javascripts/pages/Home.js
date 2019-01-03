60028a28dbf7e343b21f82c0fb4e79f5d17c4e55
rnitta <attinyes@gmail.com>
2019-01-04 08:37:15 +0900

    no message

--- src/javascripts/pages/Home.js
+++ src/javascripts/pages/Home.js
@@ -8,6 +8,15 @@ class Home extends React.Component {
         <section className="mainSection-container Home-container">
           <h1>nBackTrainerとは</h1>
           <p className="px-20">iOS app</p>
+          <p className="px-20">
+            <a
+              href="https://itunes.apple.com/jp/app/nbacktrainer/id1448011827?mt=8"
+              style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2019-01-02&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;"
+            />
+          </p>
+        </section>
+        <section className="mainSection-container Home-container mt-20">
+          <h1>そのうちなにか書くつもりだった</h1>
         </section>
       </div>
     );
